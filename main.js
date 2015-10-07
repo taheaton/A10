61fa1223aa6c2c1f2256f876d97665d4574f07e4
Thomas Heaton <thomasaheaton@gmail.com>
2015-10-06 22:42:49 -0400

    commit what I have

---
+++ main.js
@@ -0,0 +1,12 @@
+$('a#S1').on('click', function(){
+  // console.log('test');
+  $('#p1').toggleClass('hide').addClass('show');
+})
+
+$('a#S2').on('click', function(){
+  $('#p2').toggleClass('hide').addClass('show');
+})
+
+$('a#S3').on('click', function(){
+  $('#p3').toggleClass('hide').addClass('show');
+})
