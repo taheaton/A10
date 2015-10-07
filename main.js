012b852e51648316ed4d0eba84beaf435dd3de33
Thomas Heaton <thomasaheaton@gmail.com>
2015-10-07 10:45:26 -0400

    corrected project

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-$('a#S1').on('click', function(){
-  // console.log('test');
-  $('#p1').toggleClass('hide').addClass('show');
-})
-
-$('a#S2').on('click', function(){
-  $('#p2').toggleClass('hide').addClass('show');
-})
-
-$('a#S3').on('click', function(){
-  $('#p3').toggleClass('hide').addClass('show');
-})
+$('section:not(:first)').addClass('hidden');
+$('.accordian').on('click', 'h2', function () {
+  $(this).parent().removeClass('hidden').siblings().addClass('hidden');
+});
